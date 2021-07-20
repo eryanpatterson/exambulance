1180cb15e0ed5af82577041b14ec2673a5c89f8b
Ethan Patterson <eryanpatterson@gmail.com>
2021-07-20 14:05:28 -0400

    Added password hashing with bcrypt

--- pages/api/user-reg.js
+++ pages/api/user-reg.js
@@ -1,19 +1,38 @@
 import { connectToDatabase } from "../../util/mongodb";
+const bcrypt = require('bcrypt');
 
 export default async (req, res) => {
     if (req.method === "POST") {
+        // Connecting to mongodb and getting data from registration page
         const { db } = await connectToDatabase();
         const data = req.body;
-
+        
+        // Hashing password
+        const saltRounds = 10;
+        const plainText = data.password;
+        console.log(plainText);
+        
         const key = { "email": data.email };
-
-        const checker = db.collection('users')
+        
+        const checker = await db.collection('users')
             .countDocuments(key);
-
+        
         if (checker !== 0) {
             res.json({msg:"User already exists"});
-        } else {     
-            const resp = await db.collection('users').insertOne(data);
+        } else {
+            await bcrypt.hash(plainText, saltRounds, function(err, hash) {
+                const user = {
+                    email: data.email,
+                    password: hash,
+                    first: data.first,
+                    last: data.last,
+                    role: data.role
+                };
+
+                 
+                const resp = db.collection('users').insertOne(user);
+            });
         }
+
     }
 };
