442d703e17a1a5638a22f6fb9b90cd1e3c0cebf0
Ethan Patterson <eryanpatterson@gmail.com>
2021-07-28 13:06:18 -0400

    Added API for handling new courses

---
+++ pages/api/add-course.js
@@ -0,0 +1,15 @@
+import { connectToDatabase } from "../../util/mongodb";
+
+export default async function addACourse(req, res) {
+    const { db } = await connectToDatabase();
+
+    const data = req.body
+
+    try {
+        const add = db.collection('courses').insertOne(data); 
+        res.status(200).send({ done: true })
+    } catch {
+        console.error(error)
+        res.status(500).end(error.message)
+    }
+}
