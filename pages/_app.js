dd38c8c875bcb742a9155c6b3a75d4571ccd6b11
Ethan Patterson <eryanpatterson@gmail.com>
2021-08-13 10:33:43 -0400

    Added redirect if no user to _app.js so it is universal

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
+import { useUser } from '../lib/hooks'
+//import Router from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+
+  useUser({ redirectTo: '/login', redirectIfFound: false })
+
   return <Component {...pageProps} />
 }
 
