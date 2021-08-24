7f5c445b1ddc4ea79415f05779bc1b6e529a7e31
Ethan Patterson <eryanpatterson@gmail.com>
2021-08-24 11:59:45 -0400

    Added container to Layout

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import utilStyles from '../styles/utils.module.css'
 
 export default function Layout({ children }) {
     return (
-        <div>
+        <div className={utilStyles.container}>
             <header>
                 <Image
                     priority
