786c3b9187a69a84f230143270952ec76499f80d
Ethan Patterson <eryanpatterson@gmail.com>
2021-08-25 21:55:43 -0400

    Updated newPrompt function to add prompt object for each student in a course

--- pages/api/prompt.js
+++ pages/api/prompt.js
@@ -1,13 +1,18 @@
+import { findCourses } from "../../lib/user";
 import { addPrompt } from "../../lib/prompts"
 
 export default async function newPrompt(req, res) {
     const data = req.body
-
-    try {
-        addPrompt(data);
-        res.status(200).send({ done: true })
-    } catch {
-        console.error(error)
-        res.status(500).send(error.message)
-    }
+    const promptCourse = await findCourses('code', data.course)
+    promptCourse[0].students.forEach(student => {
+        data.student = student
+        
+        try {
+            addPrompt(data);
+            res.status(200).send({ done: true })
+        } catch {
+            console.error(error)
+            res.status(500).send(error.message)
+        }
+    })
 }
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,12 @@ import Layout from "../components/layout";
 
 export default function Home() {
 
-    const anObject = {
-        this: 'is',
-        an: 'object'
-    }
-    console.log(anObject);
-
-    function handleClick() {
-        anObject.add = 'hello';
-        console.log(anObject)
-    }
     return (
         <Layout>
             <h1>Hello Exambulance</h1>
             <Link href="/login">
                 <a>Login</a>
             </Link>
-            <button onClick={handleClick}>Hi there</button>
             <br />
             <Link href="/register">
                 <a>Register</a>
